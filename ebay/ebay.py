3b3cb01312b34504f762b9e8ef87681366f3f28f
faisal0901 <muhfaisalll150402@gmail.com>
2022-01-20 04:05:48 +0700

    get details

---
+++ ebay/ebay.py
@@ -0,0 +1,42 @@
+
+from ast import Try
+from cmath import exp
+from bs4 import BeautifulSoup
+import requests
+
+url='https://www.ebay.com/sch/i.html'
+header={ 'User-agent':"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36"}
+params={
+    "_nkw":"laptop",
+    "_ipg":"50"
+}
+req=requests.get(url,params=params,headers=header)
+soup=BeautifulSoup(req.text,'html.parser')
+
+def get_details(link):
+    req=requests.get(link,params=params,headers=header)
+    soup=BeautifulSoup(req.text,'html.parser')
+    title_product=soup.find('h1',{'id':'itemTitle'}).find(text=True, recursive=False)
+    try:
+     total_sold=soup.find('div',{'id':'why2buy'}).find('div','w2b-cnt w2b-2 w2b-red').find('span','w2b-head').text+'sold'
+    except AttributeError:
+     total_sold='not found'
+    condition=soup.find('div',{'id':'vi-itm-cond'}).text
+    try:
+     rating=soup.find('span','reviews-star-rating')['title'][0:3]
+    except TypeError:
+     rating="rating not found" 
+    print("title item : ",title_product.encode('utf-8-sig'))
+    print("total_sold :",total_sold)
+    print("condition : ",condition)
+    print(rating)
+
+
+products=soup.find_all('li','s-item s-item__pl-on-bottom s-item--watch-at-corner')
+for product in products:
+    link =product.find('a','s-item__link')['href']
+    get_details(link)
+
+
+
+
