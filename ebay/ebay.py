8f2d8c4ea14bf87b4176f60b78b0cf1241328b3c
faisal0901 <muhfaisalll150402@gmail.com>
2022-01-20 05:57:26 +0700

    file

--- ebay/ebay.py
+++ ebay/ebay.py
@@ -1,6 +1,6 @@
 
-from ast import Try
-from cmath import exp
+
+
 from bs4 import BeautifulSoup
 import requests
 
@@ -14,25 +14,31 @@ req=requests.get(url,params=params,headers=header)
 soup=BeautifulSoup(req.text,'html.parser')
 
 def get_details(link):
+    print(link)
     req=requests.get(link,params=params,headers=header)
     soup=BeautifulSoup(req.text,'html.parser')
+    
     title_product=soup.find('h1',{'id':'itemTitle'}).find(text=True, recursive=False)
     try:
-     total_sold=soup.find('div',{'id':'why2buy'}).find('div','w2b-cnt w2b-2 w2b-red').find('span','w2b-head').text+'sold'
+     total_sold=soup.find('div',{'id':'why2buy'}).find('div','w2b-cnt w2b-2 w2b-red').find('span','w2b-head').text+' sold'
     except AttributeError:
      total_sold='not found'
     condition=soup.find('div',{'id':'vi-itm-cond'}).text
     try:
      rating=soup.find('span','reviews-star-rating')['title'][0:3]
-    except TypeError:
+    except (TypeError,AttributeError):
      rating="rating not found" 
+    price=soup.find('span',{'itemprop':'price'}).text
+    
+ 
     print("title item : ",title_product.encode('utf-8-sig'))
     print("total_sold :",total_sold)
     print("condition : ",condition)
-    print(rating)
-
-
+    print("rating :",rating)
+    print('price: ',price)
+# get_details("https://www.ebay.com/itm/185263054311?hash=item2b2289dde7:g:VUsAAOSwaQZh5xQJ")
 products=soup.find_all('li','s-item s-item__pl-on-bottom s-item--watch-at-corner')
+counter=0
 for product in products:
     link =product.find('a','s-item__link')['href']
     get_details(link)
